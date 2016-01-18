94a6304ac91c00e6d5bb2ddb70933c5c034c3e0d
Olivier JAN <olivierjan.com@gmail.com>
2016-01-18 11:33:21 +0100

    add: basic scenarios for mfr

---
+++ www.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Www(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://www.monitoring-fr.org/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_www(self):
+        driver = self.driver
+        driver.get(self.base_url)
+
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
