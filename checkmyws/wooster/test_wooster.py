80917e90fb805a632118a89791407c73a5a7d57f
Olivier JAN <olivierjan.com@gmail.com>
2016-01-20 16:23:12 +0100

    add: wooster

---
+++ checkmyws/wooster/test_wooster.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Wooster Checkmy.ws"""
+
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.expected_conditions import visibility_of
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+base_url = "https://wooster.checkmy.ws/"
+
+driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 5)
+
+class Suite(unittest.TestCase):
+    """Test wooster Website"""
+
+    def test_01_home(self):
+        """Get Home page"""
+        driver.get(base_url)
+        self.assertIn(u"Supervision & performance des sites web. - Wooster", driver.title)
+
+    def test_02_archives(self):
+        """Get Archives page"""
+        driver.find_element_by_link_text("ARCHIVES").click()
+        driver.find_element_by_xpath("//h1[text()='Archives Wooster']")
+
+    def test_03_doc(self):
+        """Get Doc page"""
+        driver.find_element_by_link_text("DOC").click()
+        driver.find_element_by_xpath("//h1[text()='Howtos, guides, didacticiels']")
+
+    def test_04_about(self):
+        """Get À Propos page"""
+        driver.find_element_by_link_text("À PROPOS").click()
+        driver.find_element_by_xpath("//h1[text()='À propos de Wooster']")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2, exit=False)
+    driver.quit()
